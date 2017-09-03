8f3012334b98e2a7ba8155cf8227e0b9d169bc66
Erica Peng <etpeng9296@gmail.com>
2017-09-03 11:25:17 -0400

    fixes to home and about page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about');
+  res.render('about', {about: true});
+});
+
+router.get('/projects', function(req, res, next) {
+
 });
 
 module.exports = router;
